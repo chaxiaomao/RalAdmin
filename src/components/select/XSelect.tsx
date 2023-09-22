ed67f45acbe40449da5d65a31ffa7b24e2fd8fb8
hcxs <1145470390@qq.com>
2023-09-23 00:50:20 +0800

    update

--- src/components/select/XSelect.tsx
+++ src/components/select/XSelect.tsx
@@ -1,27 +1,42 @@
 import './index.scss'
+import {useRef} from "react";
+
 function XSelect() {
+
+    const inputRef = useRef(null);
+
+    const handleFocusClick = () => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
+
     return (
         <div className="react-select primary css-2b097c-container">
             {/*<span aria-live="polite" aria-atomic="false" aria-relevant="additions text" className="css-7pg0cj-a11yText"></span>*/}
-            <div className="react-select__control react-select__control--is-focused react-select__control--menu-is-open css-1pahdxg-control">
+            <div
+                onClick={handleFocusClick}
+                className="react-select__control react-select__control--is-focused react-select__control--menu-is-open css-1pahdxg-control">
                 <div className="react-select__value-container css-1hwfws3">
                     <div className="react-select__placeholder css-1wa3eu0-placeholder">Single Select</div>
-                    {/*<div className="css-1g6gooi">*/}
-                    {/*    <div className="react-select__input" style={{display: "inline-block"}}>*/}
-                    {/*        <input autoCapitalize="none"*/}
-                    {/*               autoComplete="off"*/}
-                    {/*               autoCorrect="off"*/}
-                    {/*               id="react-select-4-input"*/}
-                    {/*               spellCheck="false"*/}
-                    {/*               tabIndex="0"*/}
-                    {/*               type="text"*/}
-                    {/*               aria-autocomplete="list"*/}
-                    {/*               value=""*/}
-                    {/*               className="react-select-input"*/}
-                    {/*        />*/}
-                    {/*        <div className="react-select-div"></div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div className="css-1g6gooi">
+                        <div className="react-select__input" style={{display: "inline-block"}}>
+                            <input
+                                ref={inputRef}
+                                autoCapitalize="none"
+                                   autoComplete="off"
+                                   autoCorrect="off"
+                                   id="react-select-4-input"
+                                   spellCheck="false"
+                                   tabIndex="0"
+                                   type="text"
+                                   aria-autocomplete="list"
+                                   value="xxx"
+                                   className="react-select-input"
+                            />
+                            <div className="react-select-div"></div>
+                        </div>
+                    </div>
                 </div>
                 <div className="react-select__indicators css-1wy0on6">
                     {/*<span className="react-select__indicator-separator css-1okebmr-indicatorSeparator"></span>*/}
@@ -37,20 +52,81 @@ function XSelect() {
                 </div>
             </div>
             <div className="react-select__menu css-26l3qy-menu">
-                <div className="react-select__menu-list css-11unzgr">
-                    <div className="react-select__option react-select__option--is-disabled css-165luzk-option"
-                         id="react-select-4-option-0" tabIndex="-1">Single Option
+
+                <div className="react-select__menu-list react-select__menu-list--is-multi css-11unzgr">
+                    <div
+                        className="react-select__option react-select__option--is-disabled react-select__option--is-focused css-19jh2ze-option"
+                        id="react-select-5-option-0" tabIndex="-1"> Multiple Options
                     </div>
-                    <div className="react-select__option react-select__option--is-focused css-1n7v3ny-option"
-                         id="react-select-4-option-1" tabIndex="-1">Foobar
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-1"
+                         tabIndex="-1">Paris
                     </div>
-                    <div className="react-select__option css-yt9ioa-option" id="react-select-4-option-2"
-                         tabIndex="-1">Is great
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-2"
+                         tabIndex="-1">Bucharest
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-3"
+                         tabIndex="-1">Rome
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-4"
+                         tabIndex="-1">New York
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-5"
+                         tabIndex="-1">Miami
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-6"
+                         tabIndex="-1">Piatra Neamt
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-7"
+                         tabIndex="-1">Paris
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-8"
+                         tabIndex="-1">Bucharest
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-9"
+                         tabIndex="-1">Rome
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-10"
+                         tabIndex="-1">New York
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-11"
+                         tabIndex="-1">Miami
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-12"
+                         tabIndex="-1">Piatra Neamt
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-13"
+                         tabIndex="-1">Paris
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-14"
+                         tabIndex="-1">Bucharest
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-15"
+                         tabIndex="-1">Rome
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-16"
+                         tabIndex="-1">New York
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-17"
+                         tabIndex="-1">Miami
+                    </div>
+                    <div className="react-select__option css-yt9ioa-option" id="react-select-5-option-18"
+                         tabIndex="-1">Piatra Neamt
                     </div>
                 </div>
+
+                {/*<div className="react-select__menu-list css-11unzgr">*/}
+                {/*    <div className="react-select__option react-select__option--is-disabled css-165luzk-option"*/}
+                {/*         id="react-select-4-option-0" tabIndex="-1">Single Option*/}
+                {/*    </div>*/}
+                {/*    <div className="react-select__option react-select__option--is-focused css-1n7v3ny-option"*/}
+                {/*         id="react-select-4-option-1" tabIndex="-1">Foobar*/}
+                {/*    </div>*/}
+                {/*    <div className="react-select__option css-yt9ioa-option" id="react-select-4-option-2"*/}
+                {/*         tabIndex="-1">Is great*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
             <input name="singleSelect" type="hidden" value=""/>
-
 
 
         </div>
